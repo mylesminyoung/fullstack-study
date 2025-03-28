9920f5152d6043a0e8bc1f0c8af02fbde7d8ae00
MinYoung-Jeon <myles.jeon@kurlycorp.com>
2025-03-28 12:11:55 +0900

    feat: CRUD의 update

--- one_bite_react/project2/src/App.js
+++ one_bite_react/project2/src/App.js
@@ -40,11 +40,31 @@ function App() {
     idRef.current += 1;
   };
 
+  const onUpdate = (targetId) => {
+    setTodo(
+      todo.map(
+        (it) => 
+          it.id === targetId ? { ...it, isDone: !it.isDone } : it
+        
+        // (it) => {
+        //   if (it.id === targetId) {
+        //     return {
+        //       ...it,
+        //       isDone: !it.isDone,
+        //     };
+        //   } else {
+        //     return it;
+        //   }
+        // }
+      )
+    );
+  };
+
   return (
     <div className="App">
       <Header />
       <TodoEditor onCreate={onCreate}/>
-      <TodoList todo={todo}/>
+      <TodoList todo={todo} onUpdate={onUpdate}/>
     </div>
   );
 }
--- one_bite_react/project2/src/component/TodoList.js
+++ one_bite_react/project2/src/component/TodoList.js
@@ -1,15 +1,38 @@
+import { useState } from "react";
 import TodoItem from "./TodoItem";
 import "./TodoList.css";
 
-const TodoList = ({ todo }) => {
+const TodoList = ({ todo, onUpdate }) => {
+    const [search, setSearch] = useState("");
+    const onChangeSearch = (e) => {
+        setSearch(e.target.value);
+    };
+    const getSearchResult = () => {
+        return search === ""
+            ? todo
+            : todo.filter((it) => 
+                it.content.toLowerCase().includes(search.toLowerCase())
+            );
+    };
+
     return (
         <div className="TodoList">
             <h4>Todo List</h4>
-            <input className="searchbar" placeholder="검색어를 입력하세요" />
+
+            <input 
+                value={search}
+                onChange={onChangeSearch}
+                className="searchbar" 
+                placeholder="검색어를 입력하세요" 
+            />
+
             <div className="list_wrapper">
-                {todo.map((it) => (
-                    <TodoItem key={it.id} {...it} />
+                {getSearchResult().map((it) => (
+                    <TodoItem key={it.id} {...it} onUpdate={onUpdate} />
                 ))}
+                {/* {todo.map((it) => (
+                    <TodoItem key={it.id} {...it} />
+                ))} */}
             </div>
         </div>
     );
