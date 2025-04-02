3f5c27dcd75fa8f364d478c8f0d0185447f53560
MinYoung-Jeon <myles.jeon@kurlycorp.com>
2025-04-02 14:41:57 +0900

    feat: useMemo

--- one_bite_react/project2/src/component/TodoList.js
+++ one_bite_react/project2/src/component/TodoList.js
@@ -1,12 +1,14 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import TodoItem from "./TodoItem";
 import "./TodoList.css";
 
 const TodoList = ({ todo, onUpdate, onDelete }) => {
     const [search, setSearch] = useState("");
+
     const onChangeSearch = (e) => {
         setSearch(e.target.value);
     };
+
     const getSearchResult = () => {
         return search === ""
             ? todo
@@ -15,9 +17,39 @@ const TodoList = ({ todo, onUpdate, onDelete }) => {
             );
     };
 
+    const analyzeTodo = useMemo(() => {
+        // console.log("analyzeTodo 함수 호출")
+        const totalCount = todo.length;
+        const doneCount = todo.filter((it) => it.isDone).length;
+        const notDoneCount = totalCount - doneCount;
+        return {
+            totalCount,
+            doneCount,
+            notDoneCount,
+        };
+    }, [todo]);
+    // const analyzeTodo = () => {
+    //     console.log("analyzeTodo 함수 호출")
+    //     const totalCount = todo.length;
+    //     const doneCount = todo.filter((it) => it.isDone).length;
+    //     const notDoneCount = totalCount - doneCount;
+    //     return {
+    //         totalCount,
+    //         doneCount,
+    //         notDoneCount,
+    //     };
+    // };
+    // const { totalCount, doneCount, notDoneCount } = analyzeTodo();
+    const { totalCount, doneCount, notDoneCount } = analyzeTodo;
+
     return (
         <div className="TodoList">
             <h4>Todo List</h4>
+            <div>
+                <div>총개수: {totalCount}</div>
+                <div>완료된 할 일: {doneCount}</div>
+                <div>아직 완료하지 못한 할 일: {notDoneCount}</div>
+            </div>
 
             <input 
                 value={search}
